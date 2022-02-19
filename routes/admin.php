a628c0169ca36b9abffa98107c362048e97298df
Jason <craig219@comcast.net>
2022-02-18 18:24:15 -0800

    Adim assets

--- routes/admin.php
+++ routes/admin.php
@@ -1,1 +1,8 @@
 <?php
+
+Route::middleware(['role:admin', 'auth'])->group(function() {
+    Route::get('dashboard', [
+        'as'=> 'dashboard',
+        'uses' => 'App\Http\Controllers\Admin\AdminController@index',
+    ]);
+});
