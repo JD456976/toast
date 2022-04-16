3536a1d0027b2a1b32e36d3f4d7fab6c059ae7f0
Jason <craig219@comcast.net>
2022-04-15 18:50:07 -0700

    Refactor rating from post requests to livewire

---
+++ app/Http/Livewire/BountyRating.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class BountyRating extends Component
+{
+    public function render()
+    {
+        return view('livewire.bounty-rating');
+    }
+}
